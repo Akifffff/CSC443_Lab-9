3353ca61f76b083116de95d8a53f36b659b5f10b
Akifffff <abdakif12@gmail.com>
2022-06-24 18:30:49 +0800

    commit session id transfer

--- next_page.php
+++ next_page.php
@@ -3,10 +3,13 @@
 session_start(); 
 echo "<html>";
 
-$sid = session_id();
-echo "Session ID returned by session_id() : ".$sid."\n";
-$sid = SID;
-echo "Session ID returned by SID: ".$sid."\n";
+$queryString = $_SERVER["QUERY_STRING"];
+echo "Query string of the incoming URL: ".$queryString."\n";
+
+echo "Cookies received:\n";
+foreach ($_COOKIE as $name => $value) {
+    echo " $name = $value\n";
+}
 
 $myLogin = $_SESSION["myLogin"]; 
 echo "Value of myLogin has been retrieved: ".$myLogin."\n";
