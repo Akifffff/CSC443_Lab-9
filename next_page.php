f4f60b697eb391c9456b3a2fb016717ad7029e6a
Akifffff <abdakif12@gmail.com>
2022-06-24 18:12:16 +0800

    commit next page

---
+++ next_page.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start(); 
+echo "<html>";
+
+$myLogin = $_SESSION ["myLogin"]; 
+echo "Value of myLogin has been retrieved: ".$myLogin."\n";
+
+$mycolor = $_SESSION ("myColor"); 
+echo "Value of MyColor has been retrieved: ".$myColor."\n";
+
+echo "</html>\n";
+
+?>
